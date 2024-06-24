47947f8c588b91f8186c573eb4324fe19491ac43
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-06-23 23:52:06 -0300

    Primeiro commit

---
+++ src/entities/Program.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Program {
+
+	public static void main(String[] args) {
+		/*
+		 Problema "duracao"
+Correção: https://github.com/acenelio/curso-algoritmos/blob/master/java/duracao.java
+Fazer um programa para ler uma duração de tempo em segundos, daí imprimir na tela esta duração no
+formato horas:minutos:segundos.
+Exemplo 1:
+Digite a duracao em segundos: 300
+0:5:0
+Exemplo 2:
+Digite a duracao em segundos: 12506
+3:28:26
+Exemplo 3:
+Digite a duracao em segundos: 140811
+39:6:51
+		 */
+		
+
+	}
+
+}
