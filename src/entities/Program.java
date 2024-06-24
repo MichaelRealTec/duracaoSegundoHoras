96d544e906ae0040a5514fcb05a8d4db51036da9
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-06-24 00:05:49 -0300

    Exercicio para converter segundos em Horas-minutos-segundos

--- src/entities/Program.java
+++ src/entities/Program.java
@@ -1,5 +1,8 @@
 package entities;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -18,8 +21,26 @@ Exemplo 3:
 Digite a duracao em segundos: 140811
 39:6:51
 		 */
+		// Resolução simples
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
-
+		// Variáveis
+		
+		int qtdSegundos, horas, minutos, segundos, resto;
+		
+		System.out.print("Digite a duração em segundos: ");
+		qtdSegundos = sc.nextInt();
+		
+		horas = qtdSegundos / 3600;
+		resto = qtdSegundos % 3600;
+		
+		minutos = resto / 60;
+		segundos = resto % 60;
+		
+		System.out.printf("%d:%d:%d\n", horas, minutos, segundos);
+		
+		sc.close();
 	}
 
 }
